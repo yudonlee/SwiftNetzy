9c8ec9d73375a5aae271b272003252cec4585453
yudonlee <39371835+yudonlee@users.noreply.github.com>
2023-02-16 23:59:43 +0900

    Add Localized Error

--- Sources/Enum/SNError.swift
+++ Sources/Enum/SNError.swift
@@ -11,3 +11,14 @@ enum SNError: Error {
     case invalidURL(url: URLEnable)
     case invalidHttpResponse(statusCode: Int)
 }
+
+extension SNError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL(let url):
+            return "URL \(url) is not valid"
+        case .invalidHttpResponse(let statusCode):
+            return "Http Request is not valid. Error Status code is \(statusCode)"
+        }
+    }
+}
