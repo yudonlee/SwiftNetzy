dfa44ba90429df1daed58fdc33573648cda2d1ce
yudonlee <39371835+yudonlee@users.noreply.github.com>
2023-02-16 23:59:23 +0900

    Add URLSessionDataTask making Async

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftNetzy",
     platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
+        .macOS(.v12), .iOS(.v13), .tvOS(.v15), .watchOS(.v8)
        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
--- Sources/SwiftNetzy.swift
+++ Sources/SwiftNetzy.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class SwiftNetzy {
+public final class SwiftNetzy {
     
     static func request<T: Codable>(_ type: T.Type, _ urlEnable: URLEnable, method: HTTPMethod, headers: [String: String] = [:], params: [String: String] = [:], body: [String: String] = [:]) async throws -> T {
         
@@ -30,14 +30,34 @@ final class SwiftNetzy {
         
         urlRequest.allHTTPHeaderFields = headers
         
-        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        let (data, response) = try await {
+            if #available(iOS 15, *) {
+                return try await URLSession.shared.data(for: urlRequest)
+            } else {
+                return try await self.requestURLSessionDataEscaping(for: urlRequest)
+            }
+        }()
         
         if let httpResponse = response as? HTTPURLResponse,
-           (200...399) ~= httpResponse.statusCode {
+           !(200..<300 ~= httpResponse.statusCode) {
             throw SNError.invalidHttpResponse(statusCode: httpResponse.statusCode)
         }
         
         let result = try JSONDecoder().decode(T.self, from: data)
         return result
     }
+    
+    
+    static func requestURLSessionDataEscaping(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+       try await withCheckedThrowingContinuation({ continuation in
+         URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+           if let error = error {
+             continuation.resume(throwing: error)
+           }
+           if let data = data, let response = response {
+             continuation.resume(returning: (data, response))
+           }
+         }.resume()
+       })
+     }
 }
