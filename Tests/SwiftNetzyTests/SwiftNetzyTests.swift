caaf38716befbf7ebffca4681ccebdd16d22228a
yudonlee <39371835+yudonlee@users.noreply.github.com>
2023-02-19 13:09:44 +0900

    Add Test Code for CI

--- Tests/SwiftNetzyTests/SwiftNetzyTests.swift
+++ Tests/SwiftNetzyTests/SwiftNetzyTests.swift
@@ -1,11 +1,35 @@
 import XCTest
 @testable import SwiftNetzy
 
+struct HttpBinResponseModel: Codable {
+    let url: String
+}
+
 final class SwiftNetzyTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual("Hello World", "Hello, World!")
+
+    func testGetMethod() async throws {
+        
+        let getResponse = try await SwiftNetzy.request(HttpBinResponseModel.self, "https://httpbin.org/get", method: .get)
+        
+        XCTAssertEqual(getResponse.url, "https://httpbin.org/get")
+    }
+    
+    func testPostMethod() async throws {
+        
+        let postResponse = try await SwiftNetzy.request(HttpBinResponseModel.self, "https://httpbin.org/post", method: .post)
+        
+        XCTAssertEqual(postResponse.url, "https://httpbin.org/post")
+    }
+    
+    func testPutMethod() async throws {
+        let putResponse = try await SwiftNetzy.request(HttpBinResponseModel.self, "https://httpbin.org/put", method: .put)
+        
+        XCTAssertEqual(putResponse.url, "https://httpbin.org/put")
+    }
+    
+    func testDeleteMethod() async throws {
+        let deleteResponse = try await SwiftNetzy.request(HttpBinResponseModel.self, "https://httpbin.org/delete", method: .delete)
+        
+        XCTAssertEqual(deleteResponse.url, "https://httpbin.org/delete")
     }
 }
